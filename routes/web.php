56d1655373dbaa89d2d7d52cbff65b1ed3d38418
Rakib Mahmud <fiverrrakib2017@gmail.com>
2024-06-03 23:45:37 -0700

    completed

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\Backend\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/*Backend Route*/
+Route::get('/admin/login', [AdminController::class, 'login_form'])->name('admin.login');
+Route::post('login-functionality',[AdminController::class,'login_functionality'])->name('login.functionality');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'admin'],function(){
+    Route::get('admin/logout',[AdminController::class,'logout'])->name('admin.logout');
+    Route::get('/admin/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
+    Route::post('/admin/get_dashboard_data',[AdminController::class,'get_data'])->name('admin.dashboard_get_all_data');
+
+   
 });
